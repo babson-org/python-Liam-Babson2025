86633ec8df247566b7be834942bbbb1dd373c1c5
Liam Maloney <lmaloney1@babson.edu>
2025-09-04 22:30:42 +0000

    update

--- classes/week00/second_class/second_class_exercises.py
+++ classes/week00/second_class/second_class_exercises.py
@@ -1,14 +1,25 @@
 #%%
 # print 'hello' 5 times using an arithmetic operator
+print("hello" * 5)
 
 
 #%%
 # print 'hello' 5 times using a loop
-
+for i in range(5):
+   print("hello")
 #%%
 # print 'hello' 5 times on the same line using a loop
-
+count = 0
+while count < 5:
+   count += 1
+print("hello" * count)
 #%%
+for i in range(10):
+    for j in range(10):
+      print(f"{i}{j}", end=" ")
+    print()
+
+
 ''' using nested loops print the following:
 
 00 01 02
@@ -18,17 +29,27 @@
 '''
 #%%
 # define txt and input some text from the keyboard into it
-
+txt = input()
+print(txt)
 
 #%%
 # print each letter in txt 
-
+txt =input()
+for i in range(len(txt)):
+   print(txt[i])
 #%%
 # assign the variable letter to the first letter in txt
+txt = input()
+letter = txt[0]
+print(letter)
 
 #%%
 # print out all the letters in txt that are equal to the first letter
-
+txt = input()
+letter = txt[0]
+for i in txt:
+   if i == letter:
+      print(letter)
 '''
 say txt = 'the cat in the hat was read today'
 't' is the first letter
@@ -37,6 +58,16 @@ result: tttt
 '''
 
 #%%
+myList = ['apple', 'orange', 'pear', 'blueberry', 'peach']
+length =len(myList)
+shifted_list = [None] * length
+for i, element in enumerate(myList):
+   shifted_list[i] = myList[(i + 3) % length]
+print(shifted_list)   
+
+
+
+
 '''
 # suppose we had a list of n elements. create a new list that
   shifts the elements by 3
--- classes/week02/monday/exercise/exercises_1to12.py
+++ classes/week02/monday/exercise/exercises_1to12.py
@@ -6,6 +6,17 @@ Write down the steps a program would need to make a cup of tea. Then implement a
 function make_tea() that prints each step.
 '''
 # enter your code here
+Step1 = "boil water"
+step2 = "put tea bag in water"
+step3 = "let tea steep"
+step4 ="drink tea"
+def make_tea():
+    input("do you want to make tea, Yes or No?")
+    if input.lower == "Yes":
+        print(Step1,step2,step3,step4)
+    else:
+        print("No tea for you")
+        
 
 
 pause=input('pause')
@@ -17,10 +28,22 @@ Given a list [2, 4, 6, 8, 10], write a program that prints the next three number
 (the ones after 10)
 '''
 # enter your code here
+numbers = [2, 4, 6, 8, 10]
+difference = numbers[1] - numbers[0]
+lastnumber = numbers[-1]
+print("the next three numbers are:")
+for i in range(1, 4):
+    nextnumber = lastnumber + (difference * i)
+    print(nextnumber)
+
+
+
 
 
 pause=input('pause')
 clear_screen()
+
+
 '''
 #3
 
@@ -28,7 +51,10 @@ Write a program that asks the user for their first and last name, then prints a 
 "Hello, <first name> <last name>!"
 '''
 # enter your code here
-
+def greeting():
+    firstname =input("enter your first name: ")
+    lastname = input("enter your last name:")
+    print("hello" + firstname " " + lastname +"nice to meet you!")
 
 pause=input('pause')
 clear_screen()
@@ -38,7 +64,7 @@ clear_screen()
 Write a program that prints your Python version and platform using the sys and platform modules.
 '''
 # enter your code here
-
+ 
 
 pause=input('pause')
 clear_screen()
@@ -49,7 +75,19 @@ Ask the user to input two numbers. Calculate and print their sum, difference, pr
 and division (both / and //).
 '''
 # enter your code here
-
+num1 =input("type a number")
+num2 =input("type another number")
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1/num2
+floor = num1//num2
+print("sum =" + sum)
+print("difference = " + difference)
+print("product = " + product)
+print("quotient = " + quotient)
+print("floor = " + floor)
+      
 
 pause=input('pause')
 clear_screen()
@@ -61,6 +99,13 @@ capitalized, and split it into words.
 '''
 
 # enter your code here
+sentance = input("enter a sentance: ")
+print(sentance.upper)
+print(sentance.lower)
+print(sentance.capitalize)
+print(sentance.title)
+print(sentance.split)
+
 
 
 pause=input('pause')
@@ -72,7 +117,12 @@ Calculate the result of the following without parentheses and then with parenthe
 10 + 2 * 5 / 2 - 3 ** 2
 '''
 # enter your code here
-
+calc1= 3**2
+calc2= 2*5
+calc3=calc2/2
+calc4=calc3-calc1
+final=10+calc4
+print(final)
 
 pause=input('pause')
 clear_screen()
@@ -83,6 +133,10 @@ Create a list of your three favorite foods. Replace the second item with a new o
 then print the list.
 '''
 # enter your code here
+foods = ["burger", "sushi", "steak"]
+foods[1] = "pizza"
+print("new list: " + foods)
+
 
 
 pause=input('pause')
@@ -94,6 +148,10 @@ Create a tuple with four numbers. Try to change the first number (observe the er
 and then print the tuple.
 '''
 # enter your code here
+list = (1,2,3,4,)
+list[0] = 7
+print(list)
+
 
 
 pause=input('pause')
@@ -105,6 +163,13 @@ Create a dictionary representing a student (name, age). Update the age. Create a
 favorite numbers and add a new number.
 '''
 # enter your code here
+student ={"name":"liam", "age": 19}
+student["age"] = 20
+print(student)
+favorite_numbers = [1,5,3,7,22]
+favorite_numbers.append(43)
+print(favorite_numbers)
+
 
 
 pause=input('pause')
@@ -117,6 +182,19 @@ and read it back to print it.
 '''
 # enter your code here
 
+quote =input("enter your favorite quote: ")
+
+with open("quotes.txt", "w") as file:
+    file.write(quote)
+
+with open("quote.txt", "r") as file:
+    saved_quote =file.read()
+
+print("\nQuote read from file")
+print(saved_quote)
+
+
+
 
 pause=input('pause')
 clear_screen()
@@ -125,7 +203,15 @@ clear_screen()
 Ask the user to input 5 numbers (one at a time), store them in a list, and print the sum and average.
 '''
 # enter your code here
+numbers =[]
 
+for i in range(5):
+    numb = float(input(f"enter number {i+1}: "))
+    numbers.append(numb)
+
+print("numbers: " + numbers)
+print("total: " + total)
+print("average: " + average)
 
 pause=input('pause')
 clear_screen()
